426dc668bc6f0ff4b5947a5b1542eb3fe210d3a5
Barry Ibrahima <o22105307@info04-31.iut45.univ-orleans.fr>
2024-03-22 16:59:58 +0100

    Ajout de la creation de la BD

---
+++ BD/creation.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Troncons (
+    idTroncon NUMBER PRIMARY KEY,
+    nomUsuel VARCHAR2(100),
+    distance NUMBER,
+    typeSol VARCHAR2(50),
+    deniveleMoyen NUMBER,
+    idPointDepart NUMBER,
+    idPointArrivee NUMBER,
+    FOREIGN KEY (idPointDepart) REFERENCES Etapes(idEtape),
+    FOREIGN KEY (idPointArrivee) REFERENCES Etapes(idEtape)
+);
+
+CREATE TABLE Etapes (
+    idEtape NUMBER PRIMARY KEY,
+    nomEtape VARCHAR2(100),
+    latitude NUMBER,
+    longitude NUMBER,
+    niveauConfort NUMBER,
+    idCategorieConfort NUMBER,
+    FOREIGN KEY (idCategorieConfort) REFERENCES CategoriesConfort(idCategorieConfort)
+);
+
+CREATE TABLE CategoriesConfort (
+    idCategorieConfort NUMBER PRIMARY KEY,
+    description VARCHAR2(200)
+);
